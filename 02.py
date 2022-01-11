e2e68f56da7bbf8a5b72e16751ca582df1b378d4
Saharat Kaewkheaw <79039067+imresistance@users.noreply.github.com>
2022-01-11 14:22:07 +0700

    Update 02.py
    update new code

--- 02.py
+++ 02.py
@@ -1,94 +1,45 @@
 # https://open.kattis.com/problems/busnumbers
 data = []
+merge = []
+ans = []
+
+def bus(data):
+    data = list(map(int, data))
+    data.sort()
+
+    loop = 0
+    for index in range (len(data)):
+        for i in range (loop, len(data)):
+            if len(merge) == 0:
+                merge.append(data[i])
+                if len(data) == 1:
+                    ans.append(data[i])
+                    break
+            elif len(merge) > 0:
 
 
-def checkdata (lists):
-    Number = []
-    N = int(lists[0])
-    Number = lists[1].split(' ')
-    Number = list(map(int, Number))
-    Number.sort()
-    Number = list(map(str, Number))
-    Check = 0
-    if N == len(Number) and 1 <= N <= 1000:
-        for i,num in enumerate(Number):
-            if 1 < int(num) < 1000:
-                Check += 1
-
-        seen = set()
-        uniq = []
-        for x in Number:
-            if x not in seen:
-                uniq.append(x)
-                seen.add(x)
-
-        return uniq
-    else:
-        return 0
-
-
-def bus (list):
-    Merge = []
-    Ans = []
-
-    Num = []
-    Num = checkdata(list)
-    if not Num == 0:
-        if len(Num) == 1:
-            Ans.append(Num[0])
-        else:
-            for i in range(1, len(Num)):
-                if int(Num[i]) - int(Num[i - 1]) == 1:
-                    if len(Merge) >= 0:
-                        Merge.append(Num[i - 1])
+                if data[i-1]+1 == data[i]:
+                    merge.append(data[i])
                 else:
-                    if len(Merge) == 0:
-                        Ans.append(Num[i - 1])
-                    elif len(Merge) > 0:
-                        if int(Num[i - 1]) - int(Num[i - 2]) == 1:
-                            Merge.append(Num[i - 1])
-
-                            if len(Merge) > 2:
-                                msg = str(min(Merge)) + '-' + str(max(Merge))
-                                Ans.append(msg)
-                                Merge.clear()
-                            else:
-                                for j, x in enumerate(Merge):
-                                    Ans.append(x)
-                                Merge.clear()
-                        else:
-                            Ans.append(Num[i - 1])
+                    if len(merge) > 2:
+                        msg = str((merge[0]))+'-'+str(merge[len(merge)-1])
+                        ans.append(msg)
                     else:
-                        Ans.append(Num[i])
+                        for k in range (len(merge)):
+                            ans.append(merge[k])
+                    loop += len(merge)
+                    merge.clear()
+                    break
+    stra = ' '.join(map(str, ans))
+    print(stra)
 
-                # last number
-                if i == len(Num) - 1:
-                    if len(Merge) > 0:
-                        if int(Num[i]) - int(Num[i - 1]) == 1:
-                            if len(Merge) >= 0:
-                                Merge.append(Num[i])
-
-                        if len(Merge) > 2:
-                            msg = str(min(Merge)) + '-' + str(max(Merge))
-                            Ans.append(msg)
-                            Merge.clear()
-                        else:
-                            for i, x in enumerate(Merge):
-                                Ans.append(x)
-                            Merge.clear()
-                    else:
-                        Ans.append(Num[i])
-
-                #print(Ans)
-
-        stra = ' '.join(map(str, Ans))
-        print(stra)
-
-
-
-line = input()
-data.append(line)
-for round in range(1):
-    lines = input()
-    data.append(lines)
+# input data
+count = input()
+lines = input()
+strinput = lines.split(' ')
+seen = set()
+for x in strinput:
+    if x not in seen:
+        data.append(x)
+        seen.add(x)
 bus(data)
